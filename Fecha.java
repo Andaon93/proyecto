a12e364e079c224620e1f9469022e7929092558b
Fernando <fernandojoseponcefernandez@gmail.com>
2023-05-15 16:58:03 +0200

    Modificacion

--- Fecha.java
+++ Fecha.java
@@ -1,82 +1,127 @@
-
+/**
+ * Esta clase representa una fecha y proporciona métodos para comparar y manipular fechas.
+ */
 public class Fecha implements Comparable<Fecha> {
-	private int dia;
-	private int mes;
-	private int anyo;
+    private int dia;
+    private int mes;
+    private int anyo;
 
-	public Fecha(int dia, int mes, int anyo) {
-		this.dia = dia;
-		this.mes = mes;
-		this.anyo = anyo;
-	}
+    /**
+     * Crea una instancia de Fecha con el día, mes y año especificados.
+     *
+     * @param dia  El día de la fecha.
+     * @param mes  El mes de la fecha.
+     * @param anyo El año de la fecha.
+     */
+    public Fecha(int dia, int mes, int anyo) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anyo = anyo;
+    }
 
-	public int getDia() {
-		return dia;
-	}
+    /**
+     * Obtiene el día de la fecha.
+     *
+     * @return El día de la fecha.
+     */
+    public int getDia() {
+        return dia;
+    }
 
-	public void setDia(int dia) {
-		this.dia = dia;
-	}
+    /**
+     * Establece el día de la fecha.
+     *
+     * @param dia El nuevo día de la fecha.
+     */
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
 
-	public int getMes() {
-		return mes;
-	}
+    /**
+     * Obtiene el mes de la fecha.
+     *
+     * @return El mes de la fecha.
+     */
+    public int getMes() {
+        return mes;
+    }
 
-	public void setMes(int mes) {
-		this.mes = mes;
-	}
+    /**
+     * Establece el mes de la fecha.
+     *
+     * @param mes El nuevo mes de la fecha.
+     */
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
 
-	public int getAnyo() {
-		return anyo;
-	}
+    /**
+     * Obtiene el año de la fecha.
+     *
+     * @return El año de la fecha.
+     */
+    public int getAnyo() {
+        return anyo;
+    }
 
-	public void setAnyo(int anyo) {
-		this.anyo = anyo;
-	}
+    /**
+     * Establece el año de la fecha.
+     *
+     * @param anyo El nuevo año de la fecha.
+     */
+    public void setAnyo(int anyo) {
+        this.anyo = anyo;
+    }
 
-	@Override
-	public String toString() {
-		return "" + this.dia + "/" + this.mes + "/" + this.anyo;
-	}
+    /**
+     * Devuelve una representación en formato de cadena de la fecha.
+     *
+     * @return La fecha en formato "dia/mes/año".
+     */
+    @Override
+    public String toString() {
+        return "" + this.dia + "/" + this.mes + "/" + this.anyo;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + anyo;
-		result = prime * result + dia;
-		result = prime * result + mes;
-		return result;
-	}
+    /**
+     * Calcula el código hash de la fecha.
+     *
+     * @return El código hash calculado.
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + anyo;
+        result = prime * result + dia;
+        result = prime * result + mes;
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Fecha other = (Fecha) obj;
-		if (anyo != other.anyo)
-			return false;
-		if (dia != other.dia)
-			return false;
-		if (mes != other.mes)
-			return false;
-		return true;
-	}
+    /**
+     * Compara esta fecha con otro objeto para determinar si son iguales.
+     *
+     * @param obj El objeto a comparar con esta fecha.
+     * @return true si el objeto es una fecha igual a esta, false en caso contrario.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Fecha other = (Fecha) obj;
+        if (anyo != other.anyo)
+            return false;
+        if (dia != other.dia)
+            return false;
+        if (mes != other.mes)
+            return false;
+        return true;
+    }
 
-	@Override
-	public int compareTo(Fecha otra) {
-		int res = this.anyo - otra.anyo;
-		if (res == 0) {
-			res = this.mes - otra.mes;
-			if (res == 0) {
-				res = this.dia - otra.dia;
-			}
-		}
-		return res;
-	}
-
-}
+    /**
+     * Compara esta fecha con otra fecha para determinar su orden relativo.
+    
